c0696243cb23125d23e86649078cbbf1f2528209
syeda <sbtshakeel61@gmail.com>
2024-05-27 03:11:04 +0500

    Update chalk colors

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//shebang #
 import inquirer from "inquirer";
 import chalk from "chalk";
 let totalMarks = 0;
@@ -22,20 +23,20 @@ let quizLevel = await inquirer.prompt([
         name: "options",
         type: "list",
         message: chalk.bgRedBright.bgGreenBright('\nQuiz Level:'),
-        choices: ["Basic", "Medium", "Advance"],
+        choices: [chalk.greenBright("Basic"), chalk.blueBright("Medium"), chalk.red("Advance")],
     }
 ]);
 //Basic {Quiz 3 Questions You Can Add If You Wanna More Questions}
-if (quizLevel.options === "Basic") {
+if (quizLevel.options === chalk.greenBright("Basic")) {
     const ans0 = await inquirer.prompt([
         {
             name: "ans1",
             type: "list",
             message: chalk.rgb(255, 136, 0).bold("\n\tQ1.Typescript is a _______ of javascript\n"),
-            choices: ["A.Superset", "B.Dynamic language", "C.Statics typing", "D.Generic type"],
+            choices: [chalk.grey("A.Superset"), chalk.grey("B.Dynamic language"), chalk.grey("C.Statics typing"), chalk.grey("D.Generic type")],
         }
     ]);
-    let answer = "A.Superset";
+    let answer = chalk.grey("A.Superset");
     if (ans0.ans1 === `${answer}`) { //template literals;
         totalMarks += 5;
         const ans1 = await inquirer.prompt([
@@ -43,10 +44,10 @@ if (quizLevel.options === "Basic") {
                 name: "ans2",
                 type: "list",
                 message: chalk.rgb(255, 136, 0).bold("\n\tQ2.What is TypeScript primarily used for\n"),
-                choices: ["A.Statics typing", "B.Dynamic typing", "C.Statics language", "D.Dynamic language",]
+                choices: [chalk.grey("A.Statics typing"), chalk.grey("B.Dynamic typing"), chalk.grey("C.Statics language"), chalk.grey("D.Dynamic language"),]
             }
         ]);
-        let ans = "A.Statics typing";
+        let ans = chalk.grey("A.Statics typing");
         if (ans1.ans2 === `${ans}`) {
             totalMarks += 5;
             const ans2 = await inquirer.prompt([
@@ -54,10 +55,10 @@ if (quizLevel.options === "Basic") {
                     name: "ans3",
                     type: "list",
                     message: chalk.rgb(255, 136, 0).bold("\n\tQ3.Which command would you use to install TypeScript globally using npm\n"),
-                    choices: ["A.npm install -g typescript", "B.npm i install typescript", "C.npm i typescript", "D.npx i install -g typescript",]
+                    choices: [chalk.grey("A.npm install -g typescript"), chalk.grey("B.npm i install typescript"), chalk.grey("C.npm i typescript"), chalk.grey("D.npx i install -g typescript"),]
                 }
             ]);
-            let q3 = "A.npm install -g typescript";
+            let q3 = chalk.grey("A.npm install -g typescript");
             if (ans2.ans3 === `${q3}`) {
                 totalMarks += 5;
             }
@@ -76,16 +77,16 @@ if (quizLevel.options === "Basic") {
     }
     //Medium {You Can Add If You Wanna More Questions}
 }
-else if (quizLevel.options === "Medium") {
+else if (quizLevel.options === chalk.blueBright("Medium")) {
     const Medium = await inquirer.prompt([
         {
             name: "Q1",
             type: "list",
             message: chalk.rgb(255, 136, 0).bold("\n\tQ1.How do you denote a variable as readonly in TypeScript\n"),
-            choices: ["A.Modules", "B.Readonly", "C.Varaible denote", "D.Typescript features",]
+            choices: [chalk.grey("A.Modules"), chalk.grey("B.Readonly"), chalk.grey("C.Varaible denote"), chalk.grey("D.Typescript features"),]
         }
     ]);
-    let anss = "B.Readonly";
+    let anss = chalk.grey("B.Readonly");
     if (Medium.Q1 === `${anss}`) {
         totalMarks += 5;
         const medium = await inquirer.prompt([
@@ -93,10 +94,10 @@ else if (quizLevel.options === "Medium") {
                 name: "Q2",
                 type: "list",
                 message: chalk.rgb(255, 136, 0).bold("\n\tQ2.How do you specify that a function does not return anything in TypeScript\n"),
-                choices: ["A.Function a = (){}:number", "B.Let a = func(){}", "C.function myFunc():void", "D.function myFunc(): unknown",]
+                choices: [chalk.grey("A.Function a = (){}:number"), chalk.grey("B.Let a = func(){}"), chalk.grey("C.function myFunc():void"), chalk.grey("D.function myFunc(): unknown"),]
             }
         ]);
-        let func = "C.function myFunc():void";
+        let func = chalk.grey("C.function myFunc():void");
         if (medium.Q2 === `${func}`) {
             totalMarks += 5;
             const medIum = await inquirer.prompt([
@@ -104,10 +105,10 @@ else if (quizLevel.options === "Medium") {
                     name: "m3",
                     type: "list",
                     message: chalk.rgb(255, 136, 0).bold("\n\tQ3.Which TypeScript feature allows for checking the type of a variable at runtime\n"),
-                    choices: ["A.TypeOf", "B.Type Unknown", "C.Type guardy", "D.Runtime type"]
+                    choices: [chalk.grey("A.TypeOf"), chalk.grey("B.Type Unknown"), chalk.grey("C.Type guardy"), chalk.grey("D.Runtime type")]
                 }
             ]);
-            let ansM3 = "C.Type guardy";
+            let ansM3 = chalk.grey("C.Type guardy");
             if (medIum.m3 === `${ansM3}`) {
                 totalMarks += 5;
             }
@@ -124,18 +125,18 @@ else if (quizLevel.options === "Medium") {
         console.log(chalk.red.underline(`\nTotal marks:${totalMarks--} out of 15)`));
         console.log(chalk.grey('__________________.___________________.__________________.'));
     }
-    //Advance You Can Add If You Want More Question
+    //Advance {You Can Add If You Wanna More Questions}
 }
-else if (quizLevel.options === "Advance") {
+else if (quizLevel.options === chalk.red("Advance")) {
     let ad1 = await inquirer.prompt([
         {
             name: "advance1",
             type: "list",
             message: chalk.rgb(255, 136, 0).bold("\n\tQ1.In TypeScript, what does an enum allow you to do\n"),
-            choices: ["A.Store a set of named constants, numeric or string", "B.Store a simple varibale", "C.Store a nested object", "D.Make an object with primitive type\n",]
+            choices: [chalk.grey("A.Store a set of named constants, numeric or string"), chalk.grey("B.Store a simple varibale"), chalk.grey("C.Store a nested object"), chalk.grey("D.Make an object with primitive type"),]
         }
     ]);
-    let adans = "A.Store a set of named constants, numeric or string";
+    let adans = chalk.grey("A.Store a set of named constants, numeric or string");
     if (ad1.advance1 === `${adans}`) {
         totalMarks += 5;
         const ad2 = await inquirer.prompt([
@@ -143,10 +144,10 @@ else if (quizLevel.options === "Advance") {
                 name: "advance2",
                 type: "list",
                 message: chalk.rgb(255, 136, 0).bold("\n\tQ2.How can you allow an object to have any number of properties of a given type in TypeScript\n"),
-                choices: ["A.{[key: any]: string;}", "B.{[key: string]: any;}", "C.[property: string]: string;}", "D.{[value: string]: string;}\n",]
+                choices: [chalk.grey("A.{[key: any]: string;}"), chalk.grey("B.{[key: string]: any;}"), chalk.grey("C.[property: string]: string;}"), chalk.grey("D.{[value: string]: string;}\n"),]
             }
         ]);
-        let adans2 = "B.{[key: string]: any;}";
+        let adans2 = chalk.grey("B.{[key: string]: any;}");
         if (ad2.advance2 === `${adans2}`) {
             totalMarks += 5;
             const ad3 = await inquirer.prompt([
@@ -154,10 +155,10 @@ else if (quizLevel.options === "Advance") {
                     name: "advance3",
                     type: "list",
                     message: chalk.rgb(255, 136, 0).bold("\n\tQ3.What does the extends keyword allow you to do in TypeScript\n"),
-                    choices: ["A.Add methods to an existing function", "B.Increase the value a number variable", "C.Create a subclass from a superclass", "D.Extend the length of an array\n",]
+                    choices: [chalk.grey("A.Add methods to an existing function"), chalk.grey("B.Increase the value a number variable"), chalk.grey("C.Create a subclass from a superclass"), chalk.grey("D.Extend the length of an array\n"),]
                 }
             ]);
-            let adans3 = "C.Create a subclass from a superclass";
+            let adans3 = chalk.grey("C.Create a subclass from a superclass");
             if (ad3.advance3 === `${adans3}`) {
                 totalMarks += 5;
             }
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-//shebang #!
+//shebang #
 import inquirer from "inquirer";
 import chalk from "chalk";
 let totalMarks = 0;
@@ -30,7 +30,7 @@ let quizLevel = await inquirer.prompt([
     name: "options",
     type: "list",
     message:chalk.bgRedBright.bgGreenBright('\nQuiz Level:'),
-    choices: ["Basic", "Medium", "Advance"],
+    choices: [chalk.greenBright("Basic"),chalk.blueBright("Medium"),chalk.red("Advance")],
   }
 ]);
 
@@ -38,17 +38,17 @@ let quizLevel = await inquirer.prompt([
 
 
 //Basic {Quiz 3 Questions You Can Add If You Wanna More Questions}
-if (quizLevel.options === "Basic") {
+if (quizLevel.options === chalk.greenBright("Basic")) {
   const ans0 = await inquirer.prompt([
     {
       name: "ans1",
       type: "list",
       message: chalk.rgb(255, 136, 0).bold("\n\tQ1.Typescript is a _______ of javascript\n"),
-      choices: ["A.Superset", "B.Dynamic language", "C.Statics typing","D.Generic type"],
+      choices: [chalk.grey("A.Superset"), chalk.grey("B.Dynamic language"), chalk.grey("C.Statics typing"),chalk.grey("D.Generic type")],
     }
   ]);
   
-  let answer ="A.Superset";
+  let answer =chalk.grey("A.Superset");
   
   if (ans0.ans1 === `${answer}`) {  //template literals;
     totalMarks+=5
@@ -58,11 +58,11 @@ if (quizLevel.options === "Basic") {
         name: "ans2",
         type: "list",
         message: chalk.rgb(255, 136, 0).bold("\n\tQ2.What is TypeScript primarily used for\n"),
-        choices: ["A.Statics typing","B.Dynamic typing","C.Statics language", "D.Dynamic language",]
+        choices: [chalk.grey("A.Statics typing"),chalk.grey("B.Dynamic typing"),chalk.grey("C.Statics language"), chalk.grey("D.Dynamic language"),]
       }
     ]);
     
-    let ans = "A.Statics typing";
+    let ans = chalk.grey("A.Statics typing");
     
     if (ans1.ans2 === `${ans}`) {
       totalMarks+=5;
@@ -73,11 +73,11 @@ if (quizLevel.options === "Basic") {
           type: "list",
           message:chalk.rgb(255, 136, 0).bold("\n\tQ3.Which command would you use to install TypeScript globally using npm\n"),
    
-          choices: ["A.npm install -g typescript","B.npm i install typescript","C.npm i typescript","D.npx i install -g typescript", ]
+          choices: [chalk.grey("A.npm install -g typescript"),chalk.grey("B.npm i install typescript"),chalk.grey("C.npm i typescript"),chalk.grey("D.npx i install -g typescript"), ]
         }
       ]);
    
-      let q3 = "A.npm install -g typescript";
+      let q3 = chalk.grey("A.npm install -g typescript");
       if (ans2.ans3 === `${q3}`) {
         totalMarks+=5;
       }
@@ -102,17 +102,17 @@ if (quizLevel.options === "Basic") {
 
 
 //Medium {You Can Add If You Wanna More Questions}
-} else if (quizLevel.options=== "Medium") {
+} else if (quizLevel.options === chalk.blueBright("Medium")) {
   const Medium = await inquirer.prompt([
     {
       name: "Q1",
       type: "list",
       message: chalk.rgb(255, 136, 0).bold("\n\tQ1.How do you denote a variable as readonly in TypeScript\n"),
-      choices: ["A.Modules","B.Readonly","C.Varaible denote","D.Typescript features",]
+      choices: [chalk.grey("A.Modules"),chalk.grey("B.Readonly"),chalk.grey("C.Varaible denote"),chalk.grey("D.Typescript features"),]
     }
   ]);
  
-  let anss = "B.Readonly";
+  let anss = chalk.grey("B.Readonly");
  
   if (Medium.Q1 === `${anss}`) {
     totalMarks+=5;
@@ -122,11 +122,11 @@ if (quizLevel.options === "Basic") {
         name: "Q2",
         type: "list",
         message:chalk.rgb(255, 136, 0).bold("\n\tQ2.How do you specify that a function does not return anything in TypeScript\n"),
-        choices: ["A.Function a = (){}:number","B.Let a = func(){}","C.function myFunc():void","D.function myFunc(): unknown",]
+        choices: [chalk.grey("A.Function a = (){}:number"),chalk.grey("B.Let a = func(){}"),chalk.grey("C.function myFunc():void"),chalk.grey("D.function myFunc(): unknown"),]
       }
     ]);
    
-    let func = "C.function myFunc():void";
+    let func = chalk.grey("C.function myFunc():void");
    
     if (medium.Q2 === `${func}`) {
       totalMarks+=5;
@@ -137,11 +137,11 @@ if (quizLevel.options === "Basic") {
           type: "list",
           message:chalk.rgb(255, 136, 0).bold("\n\tQ3.Which TypeScript feature allows for checking the type of a variable at runtime\n"),
           
-          choices: ["A.TypeOf", "B.Type Unknown", "C.Type guardy", "D.Runtime type"]
+          choices: [chalk.grey("A.TypeOf"),chalk.grey("B.Type Unknown"),chalk.grey("C.Type guardy"), chalk.grey("D.Runtime type")]
         }
       ]);
       
-      let ansM3 = "C.Type guardy";
+      let ansM3 = chalk.grey("C.Type guardy")
       if (medIum.m3 === `${ansM3}`) {
         totalMarks+=5;
       }
@@ -166,17 +166,17 @@ if (quizLevel.options === "Basic") {
 
 
 //Advance {You Can Add If You Wanna More Questions}
-} else if (quizLevel.options === "Advance") {
+} else if (quizLevel.options === chalk.red("Advance")) {
   let ad1 = await inquirer.prompt([
     {
       name: "advance1",
       type: "list",
       message: chalk.rgb(255, 136, 0).bold("\n\tQ1.In TypeScript, what does an enum allow you to do\n"),
-      choices: ["A.Store a set of named constants, numeric or string","B.Store a simple varibale","C.Store a nested object","D.Make an object with primitive type\n",]
+      choices: [chalk.grey("A.Store a set of named constants, numeric or string"),chalk.grey("B.Store a simple varibale"),chalk.grey("C.Store a nested object"),chalk.grey("D.Make an object with primitive type"),]
     }
   ]);
   
-  let adans = "A.Store a set of named constants, numeric or string";
+  let adans = chalk.grey("A.Store a set of named constants, numeric or string");
   if (ad1.advance1 === `${adans}`) {
     totalMarks+=5;
  
@@ -185,11 +185,11 @@ if (quizLevel.options === "Basic") {
         name: "advance2",
         type: "list",
         message: chalk.rgb(255, 136, 0).bold("\n\tQ2.How can you allow an object to have any number of properties of a given type in TypeScript\n"),
-        choices: ["A.{[key: any]: string;}","B.{[key: string]: any;}","C.[property: string]: string;}","D.{[value: string]: string;}\n",]
+        choices: [chalk.grey("A.{[key: any]: string;}"),chalk.grey("B.{[key: string]: any;}"),chalk.grey("C.[property: string]: string;}"),chalk.grey("D.{[value: string]: string;}\n"),]
       }
     ]);
   
-    let adans2 = "B.{[key: string]: any;}";
+    let adans2 = chalk.grey("B.{[key: string]: any;}");
    
     if (ad2.advance2 === `${adans2}`) {
       totalMarks+=5;
@@ -199,11 +199,11 @@ if (quizLevel.options === "Basic") {
           name: "advance3",
           type: "list",
           message:chalk.rgb(255, 136, 0).bold("\n\tQ3.What does the extends keyword allow you to do in TypeScript\n"),
-          choices: ["A.Add methods to an existing function","B.Increase the value a number variable","C.Create a subclass from a superclass","D.Extend the length of an array\n",]
+          choices: [chalk.grey("A.Add methods to an existing function"),chalk.grey("B.Increase the value a number variable"),chalk.grey("C.Create a subclass from a superclass"),chalk.grey("D.Extend the length of an array\n"),]
         }
       ]);
 
-      let adans3 = "C.Create a subclass from a superclass";
+      let adans3 =chalk.grey("C.Create a subclass from a superclass")
    
       if (ad3.advance3 === `${adans3}`) {
         totalMarks+=5;
